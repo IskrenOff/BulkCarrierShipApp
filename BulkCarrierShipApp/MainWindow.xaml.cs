21986337b713fc950d2b62e1e2f255d9a7d015de
IskrenOff <iskren.mirev@gmail.com>
2025-02-25 16:04:46 +0200

    Small changes

--- BulkCarrierShipApp/MainWindow.xaml.cs
+++ BulkCarrierShipApp/MainWindow.xaml.cs
@@ -58,11 +58,62 @@ public partial class MainWindow : Window
                 });
             }
         }
-
+        //print the ship layout
         PrintDialog printDialog = new PrintDialog();
         if (printDialog.ShowDialog()==true)
         {
             printDialog.PrintVisual(printPanel, "Ship Layout");
+        }
+    }
+
+    private void UserInputTextBox(object sender, TextChangedEventArgs e)
+    {
+        string newText = (sender as TextBox).Text;
+    }
+
+    private void PrintPreviewShipLayout(object sender, RoutedEventArgs e)
+    {
+        StackPanel printPanel = new StackPanel { Orientation = Orientation.Horizontal };
+        foreach (UIElement element in ShipPanel.Children)
+        {
+            if (element is Image)
+            {
+                printPanel.Children.Add(new Image
+                {
+                    Source = ((Image)element).Source,
+                    Margin = ((Image)element).Margin,
+                    Height = ((Image)element).Height,
+                    Width = ((Image)element).Width
+                });
+            }
+        }
+
+        PrintDialog printDialog = new PrintDialog();
+        if (printDialog.ShowDialog() == true)
+        {
+            FlowDocument doc = new FlowDocument(new Paragraph(new InlineUIContainer(printPanel)));
+            doc.Name = "FlowDoc";
+            IDocumentPaginatorSource idpSource = doc;
+            printDialog.PrintDocument(idpSource.DocumentPaginator, "Ship Layout Preview");
+        }
+    }
+
+    private void UserInputText_GotFocus(object sender, RoutedEventArgs e)
+    {
+        TextBox textBox = sender as TextBox;
+        if (textBox.Text == "Enter the ship name here")
+        {
+            textBox.Text = "";
+            textBox.Foreground = Brushes.Black;
+        }
+    }
+    private void UserInputText_LostFocus(object sender, RoutedEventArgs e)
+    {
+        TextBox textBox = sender as TextBox;
+        if (string.IsNullOrWhiteSpace(textBox.Text))
+        {
+            textBox.Text = "Enter the ship name here";
+            textBox.Foreground = Brushes.Black;
         }
     }
 }
